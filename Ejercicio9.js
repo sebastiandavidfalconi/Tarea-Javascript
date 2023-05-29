4d43de5883bf46d3ff6e26e6066553de2bbdfce6
Sebastian David Falconi Davalos <sdfalconi@espe.edu.ec>
2023-05-28 19:59:00 -0500

    Agregado comentario

--- Ejercicio9.js
+++ Ejercicio9.js
@@ -1,5 +1,6 @@
 //Crear una variable
 let variable2 = 0;
+
 //Crear una función
 function local(){
     //Crear una variable
@@ -9,5 +10,5 @@ function local(){
     console.log("Contenido de la variable local: " + variable2);
 }
 //Mostrar en consola las dos variables
-console.log("Contenido de la variable global: " + variable);
+console.log("Contenido de la variable global: " + variable); //Esto mostrará un error por el alcance de la función
 console.log("Contenido de la variable local: " + variable2);
